a17ddf2ad119dd087e24752f4ef3aad27bb5f605
Badr Helal <64813913+badr1002@users.noreply.github.com>
2021-07-10 02:26:24 +0200

    Update app.js

--- Nti_task_3/src/app.js
+++ Nti_task_3/src/app.js
@@ -7,7 +7,6 @@ const studentRouters = require('../routes/myRoutes.route');
 app.use(express.json());
 app.use(studentRouters);
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', path.join(__dirname, '../forntend/views'));
 hbs.registerPartials(path.join(__dirname, '../forntend/layouts'));
 
